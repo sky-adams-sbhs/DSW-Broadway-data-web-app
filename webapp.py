fde880eb4cbfc9ef5dbc37c7392febe310cb51bd
sky-adams <skyadams@sbunified.org>
2017-11-10 14:07:40 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -6,9 +6,26 @@ import json
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def render_about():
     return render_template('about.html')
 
+@app.route('/popularity')
+def render_popularity():
+    return render_template('popularity.html', options=get_year_options())
+    
+def get_year_options():
+    """Returns the html code for a drop down menu.  Each option is a year for which there is complete data (1990 and 2016 are missing data)."""
+    with open('broadway.json') as braodway_data:
+        weeks = json.load(broadway_data)
+    years = []
+    options = ""
+    for w in weeks:
+        year = w["Date"]["Year"]
+        if year not in years and year not = 1990 and year not = 2016:
+            years.append(year)
+            options += Markup("<option value=\"" + year + "\">" + year + "</option>")
+    return options
+    
 def is_localhost():
     """ Determines if app is running on localhost or not
     Adapted from: https://stackoverflow.com/questions/17077863/how-to-see-if-a-flask-app-is-being-run-on-localhost
