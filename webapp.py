abf4ac33437d1a5ebbb3ba7e91752d1735574577
sky-adams <skyadams@sbunified.org>
2017-11-10 14:57:20 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -12,7 +12,8 @@ def render_about():
 @app.route('/popularity')
 def render_popularity():
     if 'year' in request.args:
-        return render_template('popularitydisplay.html', options=get_year_options())
+        year = request.args['year']
+        return render_template('popularitydisplay.html', options=get_year_options(), year=year)
     return render_template('popularity.html', options=get_year_options())
 
 def get_year_options():
