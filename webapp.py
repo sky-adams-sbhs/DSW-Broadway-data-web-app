5431b06b5e8ac74ca35e9057d4dd4a46cc1dcf69
sky-adams <skyadams@sbunified.org>
2017-11-10 14:33:49 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -11,9 +11,9 @@ def render_about():
 
 @app.route('/popularity')
 def render_popularity():
-    return render_template('popularity.html', options=get_year_options())
+    return render_template('popularity.html')
 
-def get_year_options():
+"""def get_year_options():
     """Returns the html code for a drop down menu.  Each option is a year for which there is complete data (1990 and 2016 are missing data)."""
     with open('broadway.json') as braodway_data:
         weeks = json.load(broadway_data)
@@ -24,7 +24,7 @@ def get_year_options():
         if year not in years and year not = 1990 and year not = 2016:
             years.append(year)
             options += Markup("<option value=\"" + year + "\">" + year + "</option>")
-    return options
+    return options"""
  
 def is_localhost():
     """ Determines if app is running on localhost or not
